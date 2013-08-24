f3ec8c952683178ea55e87bd28dbe277cf4ec459
Daniel González Cerviño <daniel.gonzalez@freelancemadrid.es>
2013-08-24 18:11:42 +0200

    fix

--- Form/Model/NewBlogModel.php
+++ Form/Model/NewBlogModel.php
@@ -2,10 +2,10 @@
 
 /**
  * This file is part of the desarrolla2 proyect.
- * 
+ *
  * Copyright (c)
- * Daniel González Cerviño <daniel.gonzalez@freelancemadrid.es>  
- * 
+ * Daniel González Cerviño <daniel.gonzalez@freelancemadrid.es>
+ *
  * This source file is subject to the MIT license that is bundled
  * with this package in the file LICENSE.
  */
@@ -14,17 +14,23 @@ namespace Desarrolla2\Bundle\PlanetBundle\Form\Model;
 
 use Symfony\Component\Validator\Constraints as Assert;
 
-class NewBlogModel {
+/**
+ * Class NewBlogModel
+ *
+ * @author Daniel González <daniel.gonzalez@freelancemadrid.es>
+ */
+class NewBlogModel
+{
 
     /**
      * @var string $title
-     * @Assert\MinLength( limit=5 )
+     * @Assert\Length( min=5, max=255 )
      */
     public $title;
 
     /**
      * @var string $description
-     * @Assert\MinLength( limit=5 )
+     * @Assert\Length( min=5 )
      */
     public $description;
 
@@ -32,21 +38,19 @@ class NewBlogModel {
      * @var string $url
      * @Assert\NotBlank()
      * @Assert\Url()
-     * @Assert\MinLength( limit=5 )
      */
     public $url;
 
     /**
      * @var string $rss
      * @Assert\Url()
-     * @Assert\MinLength( limit=5 )
      */
     public $rss;
 
     /**
      * @var string $userName
      * @Assert\NotBlank()
-     * @Assert\MinLength( limit=3 )
+     * @Assert\Length( min=3 )
      */
     public $userName;
 
@@ -60,52 +64,99 @@ class NewBlogModel {
      */
     public $userEmail;
 
-    public function getTitle() {
+    /**
+     * @return string
+     */
+    public function getTitle()
+    {
         return $this->title;
     }
 
-    public function setTitle($title) {
+    /**
+     * @param $title
+     */
+    public function setTitle($title)
+    {
         $this->title = $title;
     }
 
-    public function getDescription() {
+    /**
+     * @return string
+     */
+    public function getDescription()
+    {
         return $this->description;
     }
 
-    public function setDescription($description) {
+    /**
+     * @param $description
+     */
+    public function setDescription($description)
+    {
         $this->description = $description;
     }
 
-    public function getUrl() {
+    /**
+     * @return string
+     */
+    public function getUrl()
+    {
         return $this->url;
     }
 
-    public function setUrl($url) {
+    /**
+     * @param $url
+     */
+    public function setUrl($url)
+    {
         $this->url = $url;
     }
 
-    public function getRss() {
+    /**
+     * @return string
+     */
+    public function getRss()
+    {
         return $this->rss;
     }
 
-    public function setRss($rss) {
+    /**
+     * @param $rss
+     */
+    public function setRss($rss)
+    {
         $this->rss = $rss;
     }
 
-    public function getUserName() {
+    /**
+     * @return string
+     */
+    public function getUserName()
+    {
         return $this->userName;
     }
 
-    public function setUserName($userName) {
+    /**
+     * @param $userName
+     */
+    public function setUserName($userName)
+    {
         $this->userName = $userName;
     }
 
-    public function getUserEmail() {
+    /**
+     * @return string
+     */
+    public function getUserEmail()
+    {
         return $this->userEmail;
     }
 
-    public function setUserEmail($userEmail) {
+    /**
+     * @param $userEmail
+     */
+    public function setUserEmail($userEmail)
+    {
         $this->userEmail = $userEmail;
     }
-
 }
