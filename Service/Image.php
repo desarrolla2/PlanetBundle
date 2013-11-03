932856f75d83b4a14f728dc1979e741d31f825cd
Daniel González Cerviño <daniel.gonzalez@freelancemadrid.es>
2013-11-03 13:59:46 +0100

    created Image Service

---
+++ Service/Image.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * This file is part of the planetubuntu project.
+ *
+ * Copyright (c)
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this package in the file LICENSE.
+ */
+
+namespace Desarrolla2\Bundle\PlanetBundle\Service;
+
+use Doctrine\ORM\EntityManager;
+use Post
+use Psr\Log\LoggerInterface;
+use Psr\Log\LogLevel;
+
+
+/**
+ * Class Image
+ *
+ * @author Daniel González <daniel.gonzalez@freelancemadrid.es>
+ */
+
+class Image extends AbstractService
+{
+
+    /**
+     *
+     * @var \Doctrine\ORM\EntityManager;
+     */
+    protected $em;
+
+    /**
+     * @param EntityManager $em
+     */
+    public function __construct(EntityManager $em)
+    {
+        $this->em = $em;
+    }
+
+
+    /**
+     *
+     */
+    public function updateImage(){
+
+    }
+}
